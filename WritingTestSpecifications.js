4ac7548facab25e348495e00ae4ff08644e8fca2
dtill8945 <dtill8945@gmail.com>
2023-05-07 10:34:00 -0500

    finished unit test section

---
+++ WritingTestSpecifications.js
@@ -0,0 +1,21 @@
+/* 
+
+Unit Tests:
+
+    1. A function called "multiplication" that returns the product of the two input numbers.
+
+        Giving the function any input that is not a number will return with an error "Incorrect input"
+        Giving the function two numbers (positive or negative) should multiply them together and return that calculation
+        Giving the function no numbers/inputs in should return with nothing
+        Giving the function only one number should return that one input
+
+    2. A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays. 
+
+        Giving the function two arrays of integers should return one array with only odd numbers in ascending order
+        Giving the function one array of integers should return all odd numbers contained in the parent array in ascending order
+        Giving the function no arrays should return with nothing
+        Giving the function anything other than an array containing integers should return with an error "Incorrect input"
+        Giving the function arrays containing multiple of the same odd number should only return that number once in the child array
+
+
+*/
